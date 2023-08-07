578abf4f7b13cf639aed90531940eca90af31a35
Hamzaali565 <ha6153806@gmail.com>
2023-08-07 20:04:43 +0500

    push

--- index.mjs
+++ index.mjs
@@ -11,7 +11,7 @@ import cors from "cors";
 import path from "path";
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 // app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
